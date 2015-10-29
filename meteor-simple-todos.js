5e2f9a547980623de69fea47f0168a9c23862d85
Martin Sojka <sojka.martin@gmail.com>
2015-10-29 16:17:54 +0100

    Initial app done and ready for test deploy. Connected to mongo, adding, checking, deleting of tasks done.

--- meteor-simple-todos.js
+++ meteor-simple-todos.js
@@ -1,23 +1,41 @@
+Tasks = new Mongo.Collection("tasks");
+
+// CLIENT
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  
+  Template.body.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  
+  Template.body.events({
+    "submit .new-task": function(event) {
+      event.preventDefault();
+      var text = event.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      event.target.text.value = "";
     }
   });
+  
+  Template.task.events({
+    "click .toggle-checked": function() {
+      Tasks.update(this._id, {
+        $set: {checked: !this.checked}
+      });
+    },
+    "click .delete": function() {
+      Tasks.remove(this._id);
+    }
+  });
+  
 }
 
+
+// SERVER
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
 }
