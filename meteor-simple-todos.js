b319a489a49a78c23a95f970c51761272a88f7a4
Martin Sojka <sojka.martin@gmail.com>
2015-10-29 17:25:48 +0100

    Move db manipulation to Meteor methods.

--- meteor-simple-todos.js
+++ meteor-simple-todos.js
@@ -23,12 +23,7 @@ if (Meteor.isClient) {
     "submit .new-task": function(event) {
       event.preventDefault();
       var text = event.target.text.value;
-      Tasks.insert({
-        text: text,
-        createdAt: new Date(),
-        owner: Meteor.userId(),
-        username: Meteor.user().username
-      });
+      Meteor.call("addTask", text);
       event.target.text.value = "";
     },
     "change .hide-completed input": function(event) {
@@ -38,12 +33,10 @@ if (Meteor.isClient) {
   
   Template.task.events({
     "click .toggle-checked": function() {
-      Tasks.update(this._id, {
-        $set: {checked: !this.checked}
-      });
+      Meteor.call("setChecked", this._id, !this.checked);
     },
     "click .delete": function() {
-      Tasks.remove(this._id);
+      Meteor.call("deleteTask", this._id);
     }
   });
   
@@ -52,6 +45,28 @@ if (Meteor.isClient) {
   });
   
 }
+
+
+// METHODS
+Meteor.methods({
+  addTask: function(text) {
+    if(!Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function(taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function(taskId, setChecked) {
+    Tasks.update(taskId, {$set: {checked: setChecked}})
+  }
+});
 
 
 // SERVER
